9cab76feed831b6bb5fcdb9f15a6800324acb442
jhshin <jhshin0422@gmail.com>
2024-04-09 17:51:26 +0900

    two seperated single liked list mergeSort() complete

--- LinkedList/Single/singleSort.py
+++ LinkedList/Single/singleSort.py
@@ -1,81 +1,97 @@
-class Node:
+class Node():
     def __init__(self, data):
         self.data = data
         self.next = None
 
-def merge_sort_linked_list(head):
-    # Base case: If the list is empty or has only one element, it's already sorted
-    if head is None or head.next is None:
-        return head
-    
-    # Find the middle of the list
-    middle = get_middle(head)
-    next_to_middle = middle.next
-    middle.next = None  # Split the list into two halves
-    
-    # Recursively sort the two halves
-    left_half = merge_sort_linked_list(head)
-    right_half = merge_sort_linked_list(next_to_middle)
-    
-    # Merge the sorted halves
-    sorted_list = merge(left_half, right_half)
-    
-    return sorted_list
+n0 = Node(3)
+n1 = Node(5)
+n2 = Node(10)
+n3 = Node(21)
 
-def get_middle(head):
-    if head is None:
-        return head
-    
-    slow = head
-    fast = head
-    while fast.next and fast.next.next:
-        slow = slow.next
-        fast = fast.next.next
-    return slow
+ptr1 = n0
+n0.next = n1
+n1.next = n2
+n2.next = n3
 
-def merge(left, right):
-    # Create a dummy node to serve as the head of the merged list
-    dummy = Node(0)
-    current = dummy
-    
-    # Merge the two sorted lists
-    while left and right:
-        if left.data < right.data:
-            current.next = left
-            left = left.next
-        else:
-            current.next = right
-            right = right.next
-        current = current.next
-    
-    # Append any remaining nodes from either list
-    if left:
-        current.next = left
-    if right:
-        current.next = right
-    
-    # Return the head of the merged list (excluding the dummy node)
-    return dummy.next
+s0 = Node(4)
+s1 = Node(6)
+s2 = Node(15)
+s3 = Node(26)
+s4 = Node(33)
 
-# Helper function to print linked list
-def print_linked_list(head):
-    current = head
-    while current:
-        print(current.data, end=" -> ")
-        current = current.next
+ptr2 = s0
+s0.next = s1
+s1.next = s2
+s2.next = s3
+s3.next = s4
+
+# printing single linked list with arrow.
+def printList(node):
+    while node is not None:
+        print(node.data, end=" -> ")
+        node = node.next
     print("None")
 
-# Example usage:
-head = Node(3)
-head.next = Node(1)
-head.next.next = Node(5)
-head.next.next.next = Node(2)
-head.next.next.next.next = Node(4)
+# function that returns single linked list's length.
+def listLen(ptr):
+    i = 0;
+    while ptr is not None:
+        i += 1
+        ptr = ptr.next
 
-print("Original linked list:")
-print_linked_list(head)
+    return i
 
-sorted_head = merge_sort_linked_list(head)
-print("\nSorted linked list:")
-print_linked_list(sorted_head)
+
+print(listLen(ptr1))
+print("")
+printList(ptr1)
+print("")
+print(listLen(ptr2))
+print("")
+printList(ptr2)
+
+
+
+def mergeSort(ptr1, ptr2):
+    
+    # if one of the ptr is empty return another ptr.
+    if ptr1 is None:
+        return ptr2
+    elif ptr2 is None:
+        return ptr1
+    
+    # not creating new head Node() method. Make the first finalPtr.
+    if (ptr1.data <= ptr2.data):
+        head = ptr1
+        ptr1 = ptr1.next
+    else:
+        head = ptr2
+        ptr2 = ptr2.next
+
+    current = head
+
+    # while loop for the connection of two single linked list.
+    while ptr1 is not None and ptr2 is not None:
+        if ptr1.data <= ptr2.data:
+            current.next = ptr1
+            ptr1 = ptr1.next
+        else:
+            current.next = ptr2
+            ptr2 = ptr2.next
+
+        current = current.next
+
+    #  If any elements remaining in list1 or list2, append them to the merged list
+    if ptr1 is not None:
+        current.next = ptr1
+    elif ptr2 is not None:
+        current.next = ptr2
+
+    # return mergeSorted list
+    return head
+
+head = mergeSort(ptr1, ptr2)
+print("")
+printList(head)
+
 
