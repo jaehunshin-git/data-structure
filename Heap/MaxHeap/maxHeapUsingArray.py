246bd3405f1706c2ec64f99bb9010ab26aedf7b7
jhshin <jhshin0422@gmail.com>
2024-06-04 22:26:52 +0900

    max heap using array

--- Heap/MaxHeap/maxHeapUsingArray.py
+++ Heap/MaxHeap/maxHeapUsingArray.py
@@ -1,4 +1,74 @@
-class MaxHeap():
-    def __init__(self) -> None:
-        pass
-    
+class MaxHeap:
+    def __init__(self):
+        self.heap = []
+
+    def parent(self, index):
+        return (index - 1) // 2
+
+    def left_child(self, index):
+        return 2 * index + 1
+
+    def right_child(self, index):
+        return 2 * index + 2
+
+    def insert(self, key):
+        self.heap.append(key)
+        self._heapify_up(len(self.heap) - 1)
+
+    def extract_max(self):
+        if len(self.heap) == 0:
+            return None
+        if len(self.heap) == 1:
+            return self.heap.pop()
+        
+        root = self.heap[0]
+        self.heap[0] = self.heap.pop()
+        self._heapify_down(0)
+        return root
+
+    def _heapify_up(self, index):
+        while index > 0 and self.heap[self.parent(index)] < self.heap[index]:
+            self.heap[self.parent(index)], self.heap[index] = self.heap[index], self.heap[self.parent(index)]
+            index = self.parent(index)
+
+    def _heapify_down(self, index):
+        largest = index
+        left = self.left_child(index)
+        right = self.right_child(index)
+
+        if left < len(self.heap) and self.heap[left] > self.heap[largest]:
+            largest = left
+
+        if right < len(self.heap) and self.heap[right] > self.heap[largest]:
+            largest = right
+
+        if largest != index:
+            self.heap[largest], self.heap[index] = self.heap[index], self.heap[largest]
+            self._heapify_down(largest)
+
+    def get_max(self):
+        if self.heap:
+            return self.heap[0]
+        return None
+
+    def size(self):
+        return len(self.heap)
+
+    def is_empty(self):
+        return len(self.heap) == 0
+
+
+
+maxHeap = MaxHeap()
+
+keySet = [10,20,5,30]
+for key in keySet:
+    maxHeap.insert(key)
+print(maxHeap.heap)
+print("Max value:", maxHeap.extract_max())  # 30
+print("Max value:", maxHeap.extract_max())  # 20
+print("Max value:", maxHeap.extract_max())  # 10
+print("Max value:", maxHeap.extract_max())  # 5
+print("Max value:", maxHeap.extract_max())  # None
+
+print(maxHeap.heap)
